49c7ff5e488c980a281afb13bc37507a364cc1d6
milk717 <robolindasoo@gmail.com>
2022-08-15 15:36:21 +0900

    arr=>object, object=>arr

--- src/javaScriptTest/object_methods.js
+++ src/javaScriptTest/object_methods.js
@@ -8,3 +8,11 @@ console.log(user.name)
 const cloneUser = Object.assign({},user);
 cloneUser.name = 'hansam';
 console.log(user, cloneUser);
+
+//객체 => 배열
+let arr = Object.entries(user);
+console.log(arr);
+
+//배열 => 객체
+let object = Object.fromEntries(arr);
+console.log(object);
