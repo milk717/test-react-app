fc19a7da33045e41985e668da403cb0ab7470634
milk717 <robolindasoo@gmail.com>
2022-08-15 15:15:47 +0900

    constructor_function: 내부 동작 방식 주석 추가

--- src/javaScriptTest/constructor_function.js
+++ src/javaScriptTest/constructor_function.js
@@ -1,9 +1,12 @@
 function User(name, age){
+    //this = {};
     this.name = name;
     this.age = age;
     this.sayName = () =>{
         console.log(this.name);
     }
+
+    //return this;
 }
 
 let user4 = new User('sumin',22);
